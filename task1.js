856a469c469fa2f56b97d2b0266c565d2b531887
Yerzhanov <dearhauer@gmail.com>
2023-04-03 19:51:48 +0600

    HTML-файл, подключен скрипт, обращение по уникальному id для правильного порядка скороговорки

---
+++ task1.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log(document.getElementById('string-3').innerHTML);
+console.log(document.getElementById('string-5').innerHTML);
+console.log(document.getElementById('string-2').innerHTML);
+console.log(document.getElementById('string-6').innerHTML);
+console.log(document.getElementById('string-4').innerHTML);
+console.log(document.getElementById('string-1').innerHTML);
